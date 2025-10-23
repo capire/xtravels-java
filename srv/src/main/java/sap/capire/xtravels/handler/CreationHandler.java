184539dabf79fdd068ab4137273c209ceb85db9c
David H. Lam <david.lam@sap.com>
2025-10-23 14:30:57 +0200

    Filter for actives

--- srv/src/main/java/sap/capire/xtravels/handler/CreationHandler.java
+++ srv/src/main/java/sap/capire/xtravels/handler/CreationHandler.java
@@ -6,23 +6,23 @@ import static com.sap.cds.services.draft.DraftService.EVENT_DRAFT_NEW;
 
 import cds.gen.travelservice.Bookings;
 import cds.gen.travelservice.Bookings_;
+import cds.gen.travelservice.TravelService;
 import cds.gen.travelservice.TravelService_;
 import cds.gen.travelservice.Travels;
 import com.sap.cds.ql.Select;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.ServiceName;
-import com.sap.cds.services.persistence.PersistenceService;
 import org.springframework.stereotype.Component;
 
 @Component
 @ServiceName(TravelService_.CDS_NAME)
 class CreationHandler implements EventHandler {
 
-  private final PersistenceService db;
+  private final TravelService service;
 
-  CreationHandler(PersistenceService db) {
-    this.db = db;
+  CreationHandler(TravelService service) {
+    this.service = service;
   }
 
   // Fill in alternative keys as consecutive numbers for new Travels, Bookings, and Supplements.
@@ -30,7 +30,11 @@ class CreationHandler implements EventHandler {
   // but on CREATE only, as multiple users could create new Travels concurrently.
   @Before(event = EVENT_CREATE)
   void calculateTravelId(final Travels travel) {
-    var result = db.run(Select.from(TRAVELS).columns(t -> t.ID().max().as("maxID")));
+    var result =
+        service.run(
+            Select.from(TRAVELS)
+                .where(t -> t.IsActiveEntity().eq(true))
+                .columns(t -> t.ID().max().as("maxID")));
     int maxId = (int) result.single().get("maxID");
     travel.setId(++maxId);
   }
@@ -38,8 +42,13 @@ class CreationHandler implements EventHandler {
   // Fill in IDs as sequence numbers -> could be automated by auto-generation
   @Before(event = EVENT_DRAFT_NEW)
   void calculateBookingPos(Bookings_ ref, final Bookings booking) {
-    var result = db.run(Select.from(ref).columns(t -> t.Pos().max().as("maxPos")));
-    var maxPos = (int) result.single().get("maxPos");
-    booking.setPos(++maxPos);
+    var result = service.run(Select.from(ref).columns(t -> t.Pos().max().as("maxPos")));
+    var maxPos = result.single().get("maxPos");
+    if (maxPos == null) {
+      booking.setPos(1);
+    } else {
+      int pos = (int) maxPos;
+      booking.setPos(++pos);
+    }
   }
 }
