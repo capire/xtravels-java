f8e24af01cf923893dfe9a797cebdf2d60a6115d
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2025-12-09 11:30:11 +0100

    add recalculate price logic for travels without bookings

--- srv/src/main/java/sap/capire/xtravels/handler/RecalculatePriceHandler.java
+++ srv/src/main/java/sap/capire/xtravels/handler/RecalculatePriceHandler.java
@@ -1,7 +1,5 @@
 package sap.capire.xtravels.handler;
 
-import static cds.gen.travelservice.TravelService_.TRAVELS;
-
 import cds.gen.travelservice.Bookings;
 import cds.gen.travelservice.Bookings_;
 import cds.gen.travelservice.TravelService;
@@ -21,9 +19,15 @@ import com.sap.cds.services.draft.DraftPatchEventContext;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.ServiceName;
-import java.math.BigDecimal;
-import java.util.function.Function;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static cds.gen.travelservice.TravelService_.TRAVELS;
+import static com.sap.cds.services.cds.CqnService.EVENT_CREATE;
 
 // Update a Travel's TotalPrice whenever its BookingFee is modified,
 // or when a nested Booking is deleted or its FlightPrice is modified,
@@ -66,6 +70,20 @@ class RecalculatePriceHandler implements EventHandler {
     service.run(
         Update.entity(travel).data(Travels.TOTAL_PRICE, totalPrice).hint("@readonly", false));
   }
+
+    @After(event = EVENT_CREATE, entity = Travels_.CDS_NAME)
+    void setTotalPriceAfterCreation(Travels travels) {
+
+        //travel is created with the total price being the booking fee in case no total price is set
+        if (travels.getTotalPrice() == null || travels.getTotalPrice().equals(BigDecimal.ZERO)) {
+            Map<String, Object> updateData = new HashMap<>();
+            updateData.put(Travels.ID, travels.getId());
+            updateData.put(Travels.TOTAL_PRICE, travels.getBookingFee());
+
+            service.run(
+                    Update.entity(Travels_.class).data(updateData).hint("@readonly", false));
+        }
+    }
 
   private Value<Number> orZero(Value<? extends Number> value) {
     return CQL.func("coalesce", value, CQL.constant(0));
